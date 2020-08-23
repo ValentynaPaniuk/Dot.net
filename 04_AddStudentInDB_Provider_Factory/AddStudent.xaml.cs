6e11a71e7a67ebdf37200e1a1d8376305e3ae62f
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2020-08-24 00:17:23 +0300

    Homework

---
+++ 04_AddStudentInDB_Provider_Factory/AddStudent.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _03_AddStudentInDB
+{
+    /// <summary>
+    /// Логика взаимодействия для AddStudent.xaml
+    /// </summary>
+    public partial class AddStudent : Window
+    {
+        
+        List<Group> groups = new List<Group>(); //Список всіх груп
+        DbConnection connection_AddStudent;
+        DbProviderFactory factory;
+
+       // SqlConnection connection_AddStudent; //Створюємо з'єднання
+        public Student student { get; set; } //Створюємо студента
+        bool addNew; //Перевірка чи додати чи видалити
+
+        //Конструктор
+        public AddStudent(Student student, bool addNew, DbConnection connection, DbProviderFactory factory)
+        {
+            InitializeComponent();
+
+            this.addNew = addNew;
+            this.student = student;
+            this.connection_AddStudent = connection;
+
+            this.factory = factory;
+
+            DbCommand command = factory.CreateCommand();
+            command.CommandText = "Select * from Groups";
+            command.Connection = connection_AddStudent;
+
+            ReadDataFromGroups(command);
+
+            cb_Groups.ItemsSource = groups;
+            if (!addNew)
+            {
+                tb_Name.Text = student.Name;
+                tb_Surname.Text = student.Surname;
+                cb_Groups.SelectedIndex = groups.FindIndex(x => x.ID == student.IdGroup);
+            }
+        }
+
+        private void ReadDataFromGroups(DbCommand command)
+        {
+            using (DbDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        groups.Add(new Group
+                        {
+                            ID = reader.GetInt32(0),
+                            Name = reader.GetString(1)
+                        });
+                    }
+                }
+            }
+        }
+
+
+        private void AddParameter(DbCommand command, string parameterName, string value)
+        {
+            DbParameter parameter = factory.CreateParameter();
+            parameter.ParameterName = parameterName;
+            parameter.Value = value;
+            command.Parameters.Add(parameter);
+        }
+
+        //Метод додавання студента
+        private void AddStudents()
+        {
+            DbCommand command = factory.CreateCommand();
+            command.CommandText = $"insert into student values(@name, @surname, @idGroup)";
+            command.Connection = connection_AddStudent;
+
+            AddParameter(command, "@name", student.Name);
+            AddParameter(command, "@surname", student.Surname);
+            AddParameter(command, "@idGroup", student.IdGroup.ToString());
+
+            command.ExecuteNonQuery();
+        }
+
+        private void UpdateStudents()
+        {
+            DbCommand command = factory.CreateCommand();
+            command.CommandText = $"update student set Name = @name, Surname = @surname, IdGroup = @idGroup where id = @id";
+            command.Connection = connection_AddStudent;
+
+            AddParameter(command, "@id", student.IdStudent.ToString());
+            AddParameter(command, "@name", student.Name);
+            AddParameter(command, "@surname", student.Surname);
+            AddParameter(command, "@idGroup", student.IdGroup.ToString());
+
+            command.ExecuteNonQuery();
+        }
+
+
+        private void Bt_Add_Click(object sender, RoutedEventArgs e)
+        {
+            student = student ?? new Student();
+
+            if (String.IsNullOrWhiteSpace(tb_Name.Text) || String.IsNullOrWhiteSpace(tb_Surname.Text) || cb_Groups.SelectedIndex == -1)
+                return;
+
+
+            student.Name = tb_Name.Text;
+            student.Surname = tb_Surname.Text;
+            string groupName = cb_Groups.SelectedItem.ToString();
+            student.IdGroup = groups.Find(x => x.Name == groupName).ID;
+
+            if (addNew)
+                AddStudents();
+            else
+                UpdateStudents();
+
+            this.DialogResult = true;
+
+        }
+    }
+}
---
+++ 04_AddStudentInDB_Provider_Factory/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_AddStudentInDB
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /*На вікно зчитати у лістбокс перелік всіх студентів бази даних University
+          Додати 3 кнопки:
+          Додати студента
+          Видалити студента
+          Оновити дані про студента
+          Коли натискаємо кнопку "Додати" - відкривається дочірнє вікно
+          з можливістю вводу необхідних даних (Ім'я, прізвище, група)
+          група - комбобокс з НАЗВАМИ груп
+          Після заповнення необхідних полів - натискаємо кнопку 
+          ОК і студент має додатись у БД
+         */
+
+        string provider = ConfigurationManager.AppSettings["provider"];
+        string connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+        DbProviderFactory factory;
+        DbConnection connection;
+
+
+        public ObservableCollection<Student> students = new ObservableCollection<Student>();
+        string sqlExpression; //команда sql буде передаватися
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            factory = DbProviderFactories.GetFactory(provider);
+            connection = factory.CreateConnection();
+
+            //Відкриваємо з'єднання
+            try
+            {
+                try
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    UpdateCollection();
+                }
+                catch (DbException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+               
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            //
+            lb_students.ItemsSource = students; //Прив'язка колекції
+                                 
+        }
+        
+        //Метод зчитування даних про студента
+        private void ReadDataFromStudent(DbCommand command)
+        {
+            students.Clear();
+            using (DbDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        students.Add(new Student
+                        {
+                            IdStudent = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Surname = reader.GetString(2),
+                            IdGroup = reader.GetValue(3) as Nullable<int>
+                        });
+                    }
+                }
+            }
+        }
+
+
+        //Оновлюємо дані про студентів
+        private void UpdateCollection()
+        {
+
+            sqlExpression = "Select * from Student";
+            DbCommand command = factory.CreateCommand();
+            command.CommandText = sqlExpression;
+            command.Connection = connection;
+            ReadDataFromStudent(command);
+        }
+
+        private void Click_Add(object sender, RoutedEventArgs e)
+        {
+           
+            AddStudent window1 = new AddStudent(new Student(), true, connection, factory);
+            if (window1.ShowDialog() == true)
+                UpdateCollection();
+
+
+        }
+
+        private void Click_Delete(object sender, RoutedEventArgs e)
+        {
+            if (lb_students.SelectedIndex == -1)
+                return;
+
+            Student st = students[lb_students.SelectedIndex];
+            DbCommand command = factory.CreateCommand();
+            command.CommandText = $"Delete Student Where Id = {st.IdStudent}";
+            command.Connection = connection;
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            UpdateCollection();
+
+        }
+
+
+
+        private void Click_Update(object sender, RoutedEventArgs e)
+        {
+            // //Якщо немає виділеного стурента
+            if (lb_students.SelectedIndex == -1)
+                return;
+
+            //інакше
+            AddStudent window1 = new AddStudent(students[lb_students.SelectedIndex], false, connection, factory);
+            if (window1.ShowDialog() == true)
+                UpdateCollection();
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connection.Close();
+        }
+
+    }
+}
