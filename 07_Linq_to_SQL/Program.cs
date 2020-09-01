b81a0434041831657c26d50fbf3eea786a41a177
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2020-09-01 18:09:53 +0300

    Classwork

---
+++ 07_Linq_to_SQL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Linq_to_SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Інсталюємо LINQ to SQL tools через Installer VS
+        }
+    }
+}
